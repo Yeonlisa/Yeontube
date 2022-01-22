6d01328369071e206af9318aa7cd22402e05fa6c
yeonlisa <tkzw4869@gmail.com>
2022-01-22 21:46:07 +0900

    deploy

--- server/index.js
+++ server/index.js
@@ -54,6 +54,8 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
+
+  app.listen(9000);
 }
 
 const port = process.env.PORT || 5000
